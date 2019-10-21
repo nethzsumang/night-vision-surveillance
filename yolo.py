34cfff94e441f7e9e2e70e1645d80806db1037de
Kenneth Sumang <kennethsumang08@gmail.com>
2019-10-21 22:07:00 +0800

    Major rehaul.

--- yolo.py
+++ yolo.py
@@ -1,75 +1,117 @@
-import cv2
 import numpy as np
+import time
+import cv2
+import os
+from imutils.video import VideoStream
+from imutils.video import FPS
 
-from app.image import Image
-from app.yolo import Network
-from app.video import Video
-from constants.yolo import LABELS, COLORS, WEIGHTS_PATH, CFGS_PATH, CONFIDENCE, THRESHOLD
+LABELS_PATH = "resources" + os.sep + "yolo" + os.sep + "coco.names"
+WEIGTHS_PATH = "resources" + os.sep + "yolo" + os.sep + "yolov3.weights"
+CFG_PATH = "resources" + os.sep + "yolo" + os.sep + "yolov3.cfg"
+LABELS = open(LABELS_PATH).read().strip().split("\n")
+CONFIDENCE = 0.5
+THRESHOLD = 0.3
 
+# initialize a list of colors to represent each possible class label
+np.random.seed(42)
+COLORS = np.random.randint(0, 255, size=(len(LABELS), 3), dtype="uint8")
 
-def start():
-    network_obj = Network(LABELS, WEIGHTS_PATH, CFGS_PATH)
-    video_obj = Video()
-    video_obj.start()
+# load our YOLO object detector trained on COCO dataset (80 classes)
+# and determine only the *output* layer names that we need from YOLO
+print("[INFO] loading YOLO from disk...")
+net = cv2.dnn.readNetFromDarknet(CFG_PATH, WEIGTHS_PATH)
+ln = net.getLayerNames()
+ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 
-    while True:
-        frame = video_obj.video_stream.read()
-        (h, w) = frame.shape[:2]
-        image_obj = Image(image=frame)
-        blob = image_obj.get_input_blob((416, 416), swapRB=True)
-        network_obj.set_input(blob)
-        layer_outputs = network_obj.run()
+# initialize the video stream, allow the cammera sensor to warmup,
+# and initialize the FPS counter
+print("[INFO] starting video stream...")
+vs = VideoStream(src=0).start()
+time.sleep(2.0)
+fps = FPS().start()
 
-        boxes = []
-        confidences = []
-        classIDs = []
+# loop over frames from the video file stream
+while True:
+	# read the next frame from the file
+	frame = vs.read()
+	(H, W) = frame.shape[:2]
 
-        for output in layer_outputs:
-            for detection in output:
-                scores = detection[5:]
-                classID = np.argmax(scores)
-                confidence = scores[classID]
+	# construct a blob from the input frame and then perform a forward
+	# pass of the YOLO object detector, giving us our bounding boxes
+	# and associated probabilities
+	blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416))
+	net.setInput(blob)
+	start = time.time()
+	layerOutputs = net.forward(ln)
+	end = time.time()
 
-                if confidence > CONFIDENCE:
-                    box = detection[0:4] * np.array([w, h, w, h])
-                    (centerX, centerY, width, height) = box.astype("int")
+	# initialize our lists of detected bounding boxes, confidences,
+	# and class IDs, respectively
+	boxes = []
+	confidences = []
+	classIDs = []
 
-                    # use the center (x, y)-coordinates to derive the top
-                    # and and left corner of the bounding box
-                    x = int(centerX - (width / 2))
-                    y = int(centerY - (height / 2))
+	# loop over each of the layer outputs
+	for output in layerOutputs:
+		# loop over each of the detections
+		for detection in output:
+			# extract the class ID and confidence (i.e., probability)
+			# of the current object detection
+			scores = detection[5:]
+			classID = np.argmax(scores)
+			confidence = scores[classID]
 
-                    # update our list of bounding box coordinates,
-                    # confidences, and class IDs
-                    boxes.append([x, y, int(width), int(height)])
-                    confidences.append(float(confidence))
-                    classIDs.append(classID)
+			# filter out weak predictions by ensuring the detected
+			# probability is greater than the minimum probability
+			if confidence > CONFIDENCE:
+				# scale the bounding box coordinates back relative to
+				# the size of the image, keeping in mind that YOLO
+				# actually returns the center (x, y)-coordinates of
+				# the bounding box followed by the boxes' width and
+				# height
+				box = detection[0:4] * np.array([W, H, W, H])
+				(centerX, centerY, width, height) = box.astype("int")
 
-            idxs = cv2.dnn.NMSBoxes(boxes, confidences, CONFIDENCE, THRESHOLD)
+				# use the center (x, y)-coordinates to derive the top
+				# and and left corner of the bounding box
+				x = int(centerX - (width / 2))
+				y = int(centerY - (height / 2))
 
-            if len(idxs) > 0:
-                # loop over the indexes we are keeping
-                for i in idxs.flatten():
-                    # extract the bounding box coordinates
-                    (x, y) = (boxes[i][0], boxes[i][1])
-                    (w, h) = (boxes[i][2], boxes[i][3])
+				# update our list of bounding box coordinates,
+				# confidences, and class IDs
+				boxes.append([x, y, int(width), int(height)])
+				confidences.append(float(confidence))
+				classIDs.append(classID)
 
-                    # draw a bounding box rectangle and label on the frame
-                    color = [int(c) for c in COLORS[classIDs[i]]]
-                    cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
-                    text = "{}: {:.4f}".format(LABELS[classIDs[i]],
-                                               confidences[i])
-                    cv2.putText(frame, text, (x, y - 5),
-                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+	# apply non-maxima suppression to suppress weak, overlapping
+	# bounding boxes
+	idxs = cv2.dnn.NMSBoxes(boxes, confidences, CONFIDENCE, THRESHOLD)
 
-        cv2.imshow("Frame", frame)
-        key = cv2.waitKey(1) & 0xFF
+	# ensure at least one detection exists
+	if len(idxs) > 0:
+		# loop over the indexes we are keeping
+		for i in idxs.flatten():
+			# extract the bounding box coordinates
+			(x, y) = (boxes[i][0], boxes[i][1])
+			(w, h) = (boxes[i][2], boxes[i][3])
 
-        # if the `q` key was pressed, break from the loop
-        if key == ord("q"):
-            break
+			# draw a bounding box rectangle and label on the frame
+			color = [int(c) for c in COLORS[classIDs[i]]]
+			cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+			text = "{}: {:.4f}".format(LABELS[classIDs[i]], confidences[i])
+			cv2.putText(frame, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
 
-        # update the FPS counter
-        video_obj.fps.update()
+	# show the output frame
+	cv2.imshow("Frame", frame)
+	key = cv2.waitKey(1) & 0xFF
 
-    video_obj.video_stream.stop()
+	# if the `q` key was pressed, break from the loop
+	if key == ord("q"):
+		break
+
+	# update the FPS counter
+	fps.update()
+
+# release the file pointers
+print("[INFO] cleaning up...")
+vs.release()
