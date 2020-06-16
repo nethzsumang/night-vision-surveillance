d7800735d1cabdb1aebca650a1e7d75b521dfe5e
Kenneth Sumang <kennethsumang08@gmail.com>
2020-06-16 15:27:43 +0800

    fix: Set daemon thread to True

--- app/models/Thread.py
+++ app/models/Thread.py
@@ -4,6 +4,7 @@ import time
 
 class Thread(threading.Thread):
     def __init__(self, process, args, threadID, name, delay=1):
+        self.daemon = True
         threading.Thread.__init__(self)
         self.threadID = threadID
         self.name = name
