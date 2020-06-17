2f8e389cb1ee59b4ce47927f1c9519eaea128975
Kenneth Sumang <kennethsumang08@gmail.com>
2020-06-17 08:11:43 +0800

    feat: Optimize alert.

--- app/services/AlertService.py
+++ app/services/AlertService.py
@@ -5,9 +5,9 @@ class AlertService:
     def __init__(self, config):
         self.config = config
         pygame.mixer.init()
+        pygame.mixer.music.load("Alarm.mp3")
 
     def alert(self):
         if self.config["audio"]["audio_alert"] == 1:
             pygame.mixer.stop()
-            pygame.mixer.music.load("Alarm.mp3")
             pygame.mixer.music.play()
